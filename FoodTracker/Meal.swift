6b5920a537ea82021cbb02a1e637cf1841510cd7
TudorTacal <tudortacal@gmail.com>
2016-12-27 22:17:34 +0000

    Defined the properties. Initialized the class with the parameter values

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 Tacal Tudor. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    
+    init(name: String, photo: UIImage?, rating: Int) {
+    
+        //Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    
+    }
+}
