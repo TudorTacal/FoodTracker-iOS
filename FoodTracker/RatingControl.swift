faa5d0502e69d65de9a25e08f245cdc536ee75d7
TudorTacal <tudortacal@gmail.com>
2016-12-27 13:16:15 +0000

    Defined property observers for star count and star size

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,6 +12,19 @@ import UIKit
     
     var rating = 0
     
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        
+        didSet {
+            setupButtons()
+        }
+        
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
     //MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,15 +44,15 @@ import UIKit
     //MARK: Private Methods
     
     private func setupButtons() {
-        for _ in 0..<5 {
+        for _ in 0..<starCount {
             //Create the button
             let button = UIButton()
             button.backgroundColor = UIColor.red
             
             //Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             //Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
