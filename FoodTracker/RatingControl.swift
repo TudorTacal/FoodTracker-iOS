156ef9c8ea45370bbe89a7ae39bfe4545bdbb1b8
TudorTacal <tudortacal@gmail.com>
2016-12-27 11:45:47 +0000

    Inititalized the new class

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,21 +1,20 @@
-//
-//  RatingControl.swift
-//  FoodTracker
-//
-//  Created by Tacal Tudor on 27/12/2016.
-//  Copyright © 2016 Tacal Tudor. All rights reserved.
-//
+
 
 import UIKit
 
 class RatingControl: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+   
+
+
+ 
 
 }
