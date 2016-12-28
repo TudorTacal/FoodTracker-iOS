fafa34d12779734cf0b99323c831ec24435049e6
TudorTacal <tudortacal@gmail.com>
2016-12-28 11:56:11 +0000

    Displayed the view

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
     
+   
+    
     //MARK: Properties
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var ratingControl: RatingControl!
