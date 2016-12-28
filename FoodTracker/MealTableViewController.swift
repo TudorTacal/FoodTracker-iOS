ba5b437c3ad536e5ea03d322ba25eb3e018caf50
TudorTacal <tudortacal@gmail.com>
2016-12-28 12:02:03 +0000

    Refactored and cleaned up the project

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -112,15 +112,15 @@ class MealTableViewController: UITableViewController {
         let photo2 = UIImage(named: "meal2")
         let photo3 = UIImage(named: "meal3")
         
-        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+        guard let meal1 = Meal(name: "Supreme pizza with eggs", photo: photo1, rating: 4) else {
             fatalError("Unable to instantiate meal")
         }
         
-        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+        guard let meal2 = Meal(name: "Hungarian goulash", photo: photo2, rating: 5) else {
             fatalError("Unable to instantiate meal2")
         }
         
-        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+        guard let meal3 = Meal(name: "Incredible waffles", photo: photo3, rating: 3) else {
             fatalError("Unable to instantiate meal2")
         }
         
