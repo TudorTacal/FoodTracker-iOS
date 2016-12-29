d82cae9cb925e81022d220531780eb89b748c6db
TudorTacal <tudortacal@gmail.com>
2016-12-29 20:37:45 +0000

    Implemented the NSCoding protocol

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -7,10 +7,37 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+
+class Meal: NSObject, NSCoding {
     
     //MARK: Properties
+    
+    //Mark: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    encode(with aCoder: NSCoder)
+    required convenience init?(coder aDecoder: NSCoder) {
+        //The name is required. If we cannot decode a name string, the intializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String
+            else {
+                os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+                return nil
+        }
+        //Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey:PropertyKey.rating)
+        
+        //MUst call designated initializer.\
+        self.init(name: name, photo: photo, rating: rating)
+    }
     
     var name: String
     var photo: UIImage?
@@ -37,5 +64,12 @@ class Meal {
         self.photo = photo
         self.rating = rating
     
+    }
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+        
     }
 }
