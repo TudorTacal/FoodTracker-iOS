5358d26f227e5ce41c220bff4103ba611eb15d6f
TudorTacal <tudortacal@gmail.com>
2016-12-29 18:52:11 +0000

    Implemented the logic behing the seques

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import os.log
+
 
 class MealTableViewController: UITableViewController {
     //MARK: Properties
@@ -95,16 +97,36 @@ class MealTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        super.prepare(for: seque, sender: sender)
+        switch(seque.identifier ?? "") {
+            case "AddItem":
+                os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+            case "ShowDetail":
+                guard let mealDetailViewController = segue.destination as? MealViewController
+                    else {
+                        fatalError("Unexpected destination: \(segue.destination)")
+            }
+                guard let selectedMealCell = sender as? MealTableViewCell else {
+                    fatalError("Unexpected sender: \(sender)")
+            }
+                guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                    fatalError("The selected cell is not being displayed by the table")
+                 
+            }
+                let selectedMeal = meals[indexPath.row]
+                mealDetailViewController.meal = selectedMeal
+        default:
+            fatalError("Unexpected Seque Identifier; \(segue.indentifier)")
+            
+
+        }
     }
-    */
-    
+ 
     //MARK: Actions 
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         if let sourceViewController = sender.source as?
